3f9bd6c7e7406ef4a1c8a986af2af9b844c86f2c
dalabbas <duha.alabbasi05@gmail.com>
2025-03-17 23:22:22 +0300

    Added comments and fixed parts of code

--- App.js
+++ App.js
@@ -6,9 +6,9 @@ import { Ionicons } from '@expo/vector-icons'; // Provides the icons needed for 
 
 // The function below defines the main component, where it defines the state of the task name, description, date to be done, and priority
 export default function App() {
-  const [task, setTask] = useState('');
-  const [description, setDescription] = useState('');
-  const [selectedDate, setSelectedDate] = useState('');
+  const [task, setTask] = useState(''); // default value of task is empty string
+  const [description, setDescription] = useState(''); // default value of description is empty string
+  const [selectedDate, setSelectedDate] = useState(''); // default value of selected date is empty string
   const [priority, setPriority] = useState(0); // Priority in this case is 0 (no priority) there are three total levels (1,2,3) which indicate low, medium, and high
 
   // The code below updates the "setSelectedDate" constant to the selected date in YYYY-MM-DD format
@@ -28,7 +28,7 @@ export default function App() {
       <TextInput
         style={styles.input}
         value={task} // binds input to task state
-        placeholder="Task Name"
+        placeholder="Input the task name!" // this will be viewed in the text box
         onChangeText={setTask} // This will update the state of "task" based on what the user inputted
       />
 
@@ -36,16 +36,17 @@ export default function App() {
       <Text style={styles.label}>Description:</Text>
       <TextInput
         style={styles.input}
-        placeholder="Describe the task!"
-        value={description}
-        onChangeText={setDescription}
+        placeholder="Describe the task!" // this will be viewed in the description textbox
+        value={description} 
+        onChangeText={setDescription} // when the text is inputted, this function will be called to set the description value 
       />
 
       <Text style={styles.dateLabel}>Date: {selectedDate}</Text>
       <Calendar
         onDayPress={onDayPress} // Calls the onDayPress function when a date is selected to make it in the specified YYY-MM-DD format 
-        markedDates={{ [selectedDate]: { selected: true, marked: true, selectedColor: 'rgba(78, 197, 197, 1)' } }}
+        markedDates={{ [selectedDate]: { selected: true, marked: true, selectedColor: 'rgba(78, 197, 197, 1)' } }} // when a date is selected, its color will be changed to teal
         theme={{
+          // The following code below formats colors of specific components in the calendar 
           selectedDayBackgroundColor: 'rgba(78, 197, 197, 1)',
           arrowColor: '#333',
           monthTextColor: '#333', 
@@ -58,6 +59,7 @@ export default function App() {
       <View style={styles.priorityRow}>
         <Text style={styles.priorityLabel}>Set Priority:</Text>
         <TouchableOpacity
+        // this applies the style for the priority button (low/green) and changes the value of priority if the button was pressed on
           style={[styles.priorityButton, priority === 1 && styles.selectedPriority(1)]}
           onPress={() => prioritySelect(1)} 
         >
@@ -65,6 +67,7 @@ export default function App() {
         </TouchableOpacity>
 
         <TouchableOpacity
+         // this applies the style for the priority button (med/orange) and changes the value of priority if the button was pressed on
           style={[styles.priorityButton, priority === 2 && styles.selectedPriority(2)]}
           onPress={() => prioritySelect(2)} 
         >
@@ -72,6 +75,7 @@ export default function App() {
         </TouchableOpacity>
 
         <TouchableOpacity
+         // this applies the style for the priority button (high/red) and changes the value of priority if the button was pressed on
           style={[styles.priorityButton, priority === 3 && styles.selectedPriority(3)]}
           onPress={() => prioritySelect(3)} 
         >
@@ -80,7 +84,8 @@ export default function App() {
       </View>
 
       <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Link to Child</Text>
+        {/* This views the last two buttons in the page, which are the "Link to child" button and "Save" button */}
+        <Text style={styles.buttonText}>Link to Child</Text> 
       </TouchableOpacity>
       <TouchableOpacity style={styles.saveButton}>
         <Text style={styles.buttonText}>Save</Text>
@@ -89,7 +94,7 @@ export default function App() {
     </View>
   );
 }
-
+// The following code below defines the style (color, appearance, alignment, etc.) of each component in the UI
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
@@ -120,7 +125,7 @@ const styles = StyleSheet.create({
   priorityLabel: {
     fontSize: 16,
     color: '#333',
-    marginRight: 10, // Reduced margin for buttons to be close to text
+    marginRight: 10, 
   },
   priorityRow: {
     flexDirection: 'row',
